3992e19f246b777b14fcb7c11df95f52e3a92b7d
Kurtina <kurtdekker@gmail.com>
2015-11-29 09:54:13 -0800

    Player Input is functional

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 Unity3D Pong Solitaire Tutorial
 
-by Kurt Dekker
-
-http://www.plbm.com/ - http://www.twitter.com/kurtdekker
+by Kurt Dekker - http://www.plbm.com/ - http://www.twitter.com/kurtdekker
 
 This repository is a tutorial for Pong solitaire (one paddle, one ball, three walls).
+
+Unity 5.2.2f1 - kicked off November 29, 2015
 
 Let's do it in one scene with one script.
 
--- pong_solitaire/Assets/MainGameController.cs
+++ pong_solitaire/Assets/MainGameController.cs
@@ -26,9 +26,28 @@ public class MainGameController : MonoBehaviour
 		}
 	}
 
+	Camera ourCamera;
 	void Start()
 	{
+		ourCamera = Camera.main;
 		StartCoroutine (UpdateTheScoreAndHighScore ());
+
+		SetupGameColors ();
+	}
+
+	Material mtl_borders;
+	Material mtl_paddle;
+	Material mtl_back;
+	
+	void SetupGameColors()
+	{
+		mtl_borders = new Material(Shader.Find("Diffuse"));
+		mtl_back = new Material(Shader.Find("Diffuse"));
+		mtl_paddle = new Material(Shader.Find("Diffuse"));
+		
+		mtl_borders.color = new Color (1.0f, 0.5f, 0.5f);
+		mtl_back.color = new Color (0.8f, 1.0f, 1.0f);
+		mtl_paddle.color = new Color (0.4f, 0.8f, 0.8f);
 	}
 
 	void ChangeModeToGreeting()
@@ -67,8 +86,13 @@ public class MainGameController : MonoBehaviour
 		}
 	}
 
+	// relating to the playfield area
+	const float thickness = 0.25f;
+	const float halfThick = thickness / 2;
 	const float zDepth = 5.0f;
 	GameObject playfield;
+	float width,height;
+
 	void FabricateThePlayfield()
 	{
 		if (playfield)
@@ -80,42 +104,50 @@ public class MainGameController : MonoBehaviour
 
 		// Calculate playfield height based on how far back we are from
 		// the camera and the camera's fieldOfView.
-		float height = Mathf.Tan (Camera.main.fieldOfView * Mathf.Deg2Rad * 0.5f) *
+		height = Mathf.Tan (ourCamera.fieldOfView * Mathf.Deg2Rad * 0.5f) *
 			Mathf.Abs ( Camera.main.transform.position.z - playfield.transform.position.z);
 
 		// match playfield size to screen aspect
-		float width = (height * Screen.width) / Screen.height;
+		width = (height * Screen.width) / Screen.height;
 
 		// top edge
 		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 		cube.name = "Top";
+		cube.GetComponent<Renderer> ().material = mtl_borders;
 		cube.transform.parent = playfield.transform;
-		cube.transform.localPosition = Vector3.up * height;
-		cube.transform.localScale = new Vector3 (width * 2, 1, zDepth);
+		cube.transform.localPosition = Vector3.up * (height + halfThick);
+		cube.transform.localScale = new Vector3 ((width + thickness) * 2, thickness, zDepth);
 		// and the bottom
 		cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 		cube.name = "Bottom";
+		cube.GetComponent<Renderer> ().material = mtl_borders;
 		cube.transform.parent = playfield.transform;
-		cube.transform.localPosition = Vector3.down * height;
-		cube.transform.localScale = new Vector3 (width * 2, 1, zDepth);
+		cube.transform.localPosition = Vector3.down * (height + halfThick);
+		cube.transform.localScale = new Vector3 ((width + thickness) * 2, thickness, zDepth);
 		// far right wall
 		cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 		cube.name = "Right";
+		cube.GetComponent<Renderer> ().material = mtl_borders;
 		cube.transform.parent = playfield.transform;
-		cube.transform.localPosition = Vector3.right * width;
-		cube.transform.localScale = new Vector3 (1.0f, height * 2, zDepth);
+		cube.transform.localPosition = Vector3.right * (width + halfThick);
+		cube.transform.localScale = new Vector3 (thickness, height * 2, zDepth);
 		// back wall
 		cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 		cube.name = "BackWall";
+		cube.GetComponent<Renderer> ().material = mtl_back;
 		cube.transform.parent = playfield.transform;
 		cube.transform.localPosition = Vector3.forward * zDepth * 0.5f;
-		cube.transform.localScale = new Vector3 (width * 2, height * 2, 1.0f);
+		cube.transform.localScale = new Vector3 (width * 2, height * 2, thickness);
 	}
 
 	void InitGame()
 	{
 		FabricateThePlayfield ();
 		score = 0;
+
+		playerPaddle = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		playerPaddle.transform.localScale = new Vector3 (playerPaddleXSize, playerPaddleYZSize, playerPaddleYZSize);
+		playerPaddle.GetComponent<Renderer> ().material = mtl_paddle;
 
 		StartCoroutine (SequenceForGameOver ());
 	}
@@ -135,6 +167,48 @@ public class MainGameController : MonoBehaviour
 		ChangeModeToGreeting ();
 	}
 
+	GameObject playerPaddle;
+	float playerXPositionFractionOfWidth = -0.80f;
+	float playerPaddleSizeFractionOfHeight = 0.15f;
+	const float playerPaddleXSize = thickness;
+	float playerPaddleYZSize
+	{
+		get
+		{
+			return height * playerPaddleSizeFractionOfHeight;
+		}
+	}
+
+	void GatherPlayerMouseInput()
+	{
+		Plane plane = new Plane (Vector3.back, playfield.transform.position);
+		float enter;
+		Ray ray = ourCamera.ScreenPointToRay (Input.mousePosition);
+		if (plane.Raycast ( ray, out enter))
+		{
+			Vector3 playerPosition = ray.GetPoint ( enter);
+
+			// jam at at the left edge where you belong
+			playerPosition.x = width * playerXPositionFractionOfWidth;
+
+			// constrain top and bottom
+			if (playerPosition.y > height - playerPaddleYZSize / 2)
+			{
+				playerPosition.y = height - playerPaddleYZSize / 2;
+			}
+			if (playerPosition.y < -height - playerPaddleYZSize / 2)
+			{
+				playerPosition.y = -height - playerPaddleYZSize / 2;
+			}
+			playerPaddle.transform.position = playerPosition;
+		}
+	}
+
+	void PlayOneFrame()
+	{
+		GatherPlayerMouseInput();
+	}
+
 	void Update()
 	{
 		switch( mainGameMode)
@@ -149,6 +223,7 @@ public class MainGameController : MonoBehaviour
 			}
 			break;
 		case MainGameMode.PLAYING :
+			PlayOneFrame();
 			break;
 		}
 	}
